00f49eb3b30fcc2df8dff0e1447dfec7eb4d6946
DevDemba <m.demba.dia@gmail.com>
2018-10-12 09:54:25 +0200

    Initial Commit
    first commit on Angular-Tour-Of-Heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    {id: 11, name:'Robocop'},
+    {id: 12, name:'Cesar'},
+    {id: 13, name:'Tarzan'},
+    {id: 14, name:'Peter pan'},
+    {id: 15, name:'Spider'},
+    {id: 16, name:'Batman'},
+    {id: 17, name:'Iron man'},
+    {id: 18, name:'Superman'},
+    {id: 19, name:'Terminator'},
+    {id: 20, name:'Hulk'},
+ 
+]
